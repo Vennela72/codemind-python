fc67ca331102d14f74e7cf575a3ab1076b1b6e39
Vennela72 <109405525+Vennela72@users.noreply.github.com>
2022-10-31 20:34:21 +0530

    n=int(input()) def isperfect(n):     sum=0     for x in range(1,n):         if n%x==0:             sum+=x     return sum==n print(isperfect(n))

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,8 @@
 n=int(input())
-s=0
-for i in range(1,n):
-    if n%i==0:
-        s+=i
-if s==n:
-    print('True')
-else:
-    print('False')
+def isperfect(n):
+    sum=0
+    for x in range(1,n):
+        if n%x==0:
+            sum+=x
+    return sum==n
+print(isperfect(n))
