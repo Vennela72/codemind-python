3b2f673aae93179754c457c8411c1eb4336995e9
Vennela72 <109405525+Vennela72@users.noreply.github.com>
2022-11-03 10:32:02 +0530

    n=int(input()) a=n b=n while True:     is_prime=True     for i in range(2,int(a**0.5)+1):         if a%i==0:             is_prime=False             break     if is_prime==True:         break     else:         a=a-1 while True:     is_prime=True     for i in range(2,int(b**0.5)+1):         if b%i==0:             is_prime=False             break     if is_prime==True:         break     else:         b=b+1 lp=n-a hp=b-n if lp < hp:     print(lp) else:     print(hp)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,29 @@
+n=int(input())
+a=n
+b=n
+while True:
+    is_prime=True
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            is_prime=False
+            break
+    if is_prime==True:
+        break
+    else:
+        a=a-1
+while True:
+    is_prime=True
+    for i in range(2,int(b**0.5)+1):
+        if b%i==0:
+            is_prime=False
+            break
+    if is_prime==True:
+        break
+    else:
+        b=b+1
+lp=n-a
+hp=b-n
+if lp < hp:
+    print(lp)
+else:
+    print(hp)
